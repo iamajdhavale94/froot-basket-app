e8e968b4036c0488198187fc4f70ae4381ad027d
iamajdhavale94 <iamajdhavale94@gmail.com>
2026-04-23 18:47:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,28 @@
 const cacheName = "fruitbasket-cache-v1";
 
 const filesToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/fruits.json",
-  "/manifest.json",
-  "/assets/logo.jpg",
-  "/assets/bg.jpg"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./fruits.json",
+  "./manifest.json",
+  "./assets/logo.jpg",
+  "./assets/bg.jpg",
+  "./assets/upi-qr.jpeg"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+    caches.open(cacheName).then(async (cache) => {
+      for (const file of filesToCache) {
+        try {
+          await cache.add(file);
+        } catch (err) {
+          console.log("Failed to cache:", file);
+        }
+      }
+    })
   );
 });
 
