745d4816608e625d8db1bd5d12fe0336cc57ac6f
iamajdhavale94 <iamajdhavale94@gmail.com>
2026-04-23 17:04:50 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = "fruitbasket-cache-v1";
+
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/fruits.json",
+  "/manifest.json",
+  "/assets/logo.png",
+  "/assets/bg.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
